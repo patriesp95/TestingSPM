d3d9f4d995d598ec8a868959a98ca116dca49e22
Patricia Martinez Espert <patrimespert@outlook.com>
2026-04-19 19:25:28 +0200

    Initial Commit

---
+++ TestingSPM/TestingSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingSPMApp.swift
+//  TestingSPM
+//
+//  Created by Patricia M Espert on 19/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TestingSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
